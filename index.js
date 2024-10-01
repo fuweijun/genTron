5f2edefeb701a9bc51116b429fd71c012765766e
WeijunFu <anyxx@qq.com>
2024-10-02 02:00:17 +0800

    Update index.js

--- index.js
+++ index.js
@@ -1,24 +1,28 @@
 const {TronWeb} = require('tronweb');
 var crypto = require('crypto');
-
+const maxCount = 1000;//生成个数
 async function generateAccount() {
     var privateKey = crypto.randomBytes(32).toString('hex');
-    console.log("Private Key", privateKey);
+    //console.log("Private Key", privateKey);
     // create tronweb client
-    const fullNode = 'https://api.trongrid.io';
+    const fullNode     = 'https://api.trongrid.io';
     const solidityNode = 'https://api.trongrid.io';
-    const eventServer = 'https://api.trongrid.io';
+    const eventServer  = 'https://api.trongrid.io';
     const tronWeb = new TronWeb(fullNode,solidityNode,eventServer,privateKey);
     // create new account
-    const account = tronWeb.createAccount();
-    console.log(account);
+    //const account = tronWeb.createAccount();
+    // create new mnemonic
+    const account = tronWeb.createRandom();
+    console.log(account.address,account.mnemonic.phrase);
     return true;
 }
 
 (async () => {
     try {
         console.log("start");
-        await generateAccount();
+        for (let index = 0; index < maxCount; index++) {
+            await generateAccount();
+        }
         console.log("end");
     } catch (e) {
         console.log(e);
